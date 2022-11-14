2d855ecb958f70fc59416444dc4d633ff6658990
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-11-14 16:06:44 -0600

    finished creating 1st model (orders)

--- src/models/order.ts
+++ src/models/order.ts
@@ -6,6 +6,13 @@ export type Order {
     orderStatus: String
 }
 
+export type OrderProduct {
+    id: Number
+    orderId: Number
+    productId: Number
+    quantity: Number
+}
+
 export class OrderStore {
     async index(): Promise<Order[]> {
         try {
@@ -15,55 +22,55 @@ export class OrderStore {
             connection.release()
             return result.rows
         } catch (err) {
-          throw new Error(`Cannot get orders ${err}`)
+          throw new Error(`Cannot find requested order index: ${err}`)
         }
     }
 
-    async show(id: string): Promise<Order> {
+    async show(id: number): Promise<Order> {
         try {
             const connection = await Client.connect()
-            const sql = 'SELECT * FROM orders'
-            const result = await connection.query(sql)
+            const sql = 'SELECT * FROM orders WHERE id=($1)'
+            const result = await connection.query(sql, [id])
             connection.release()
-            return result.rows
+            return result.rows[0]
         } catch (err) {
-          throw new Error(`Cannot get orders ${err}`)
+          throw new Error(`Cannot find order ${id}: ${err}`)
         }
     }
 
-    // async index(): Promise<Order[]> {
-    //     try {
-    //         const connection = await Client.connect()
-    //         const sql = 'SELECT * FROM orders'
-    //         const result = await connection.query(sql)
-    //         connection.release()
-    //         return result.rows
-    //     } catch (err) {
-    //       throw new Error(`Cannot get orders ${err}`)
-    //     }
-    // }
+    async create(order: Order): Promise<Order> {
+        try {
+            const connection = await Client.connect()
+            const sql = 'INSERT INTO orders (id, userId, orderStatus) VALUES ($1, $2, $3) RETURNING * '
+            const result = await connection.query(sql, [order.id, order.userId, order.orderStatus])
+            connection.release()
+            return result.rows[0]
+        } catch (err) {
+          throw new Error(`Unable to create order: ${err}`)
+        }
+    }
 
-    // async index(): Promise<Order[]> {
-    //     try {
-    //         const connection = await Client.connect()
-    //         const sql = 'SELECT * FROM orders'
-    //         const result = await connection.query(sql)
-    //         connection.release()
-    //         return result.rows
-    //     } catch (err) {
-    //       throw new Error(`Cannot get orders ${err}`)
-    //     }
-    // }
+    async delete(id: number): Promise<Order> {
+        try {
+            const connection = await Client.connect()
+            const sql = 'DELETE FROM orders WHERE id=($1)'
+            const result = await connection.query(sql, [id])
+            connection.release()
+            return result.rows[0]
+        } catch (err) {
+          throw new Error(`Unable to delete order ${id}: ${err}`)
+        }
+    }
 
-    // async index(): Promise<Order[]> {
-    //     try {
-    //         const connection = await Client.connect()
-    //         const sql = 'SELECT * FROM orders'
-    //         const result = await connection.query(sql)
-    //         connection.release()
-    //         return result.rows
-    //     } catch (err) {
-    //       throw new Error(`Cannot get orders ${err}`)
-    //     }
-    // }
+    async update(id: number, orderStatus: string): Promise<Order> {
+        try {
+            const connection = await Client.connect()
+            const sql = 'UPDATE orders SET orderStatus=$2 WHERE id=$1 RETURNING *'
+            const result = await connection.query(sql, [id, orderStatus])
+            connection.release()
+            return result.rows[0]
+        } catch (err) {
+          throw new Error(`Unable to update status of order ${id} to ${orderStatus}: ${err}`)
+        }
+    }
 }
