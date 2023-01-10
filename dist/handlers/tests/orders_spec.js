70f15fd75c212889151074e9bb259257af74ed5e
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2023-01-10 12:51:57 -0600

    Saving State | Still troubleshooting, learning and experimenting with test authentication

--- dist/handlers/tests/orders_spec.js
+++ dist/handlers/tests/orders_spec.js
@@ -6,8 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
 const server_1 = __importDefault(require("../../models/../server"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 dotenv_1.default.config();
-// const TokenSecret = process.env.authToken_SECRET as string
+const tokenSecret = process.env.TOKEN_SECRET;
+console.log(`tokenSecret looks like this globally: ${tokenSecret}`);
 // const testOrderStore = new OrderStore()
 // const testUserStore = new UserStore()
 const request = (0, supertest_1.default)(server_1.default);
@@ -24,18 +26,36 @@ describe('Order Enpoint Tests', () => {
         const response = await request.post('/api/users')
             .send(testUser);
         authToken = response.body;
+        console.log(`authToken BEFORE tests have run: ${authToken}`);
     });
-    it('should return 200 status for create user route', async () => {
-        const response = await request.post('/api/users')
-            .send(testUser);
-        expect(response.status).toEqual(200);
-        authToken = response.body;
+    afterAll(async () => {
+        console.log(`authToken after tests have run: ${authToken}`);
     });
-    it('should return 200 status for authenticate user route', async () => {
-        const response = await request.post('/api/users/authenticate')
-            .send(testUser)
-            .set('Authorization', authToken);
-        authToken = 'Bearer' + response.body;
-        expect(response.status).toBe(200);
+    // it(`should return 200 status for create user route AND
+    //     should return 200 status for authenticate route`, async () => {
+    //   const newTestUser = await request.post('/api/users')
+    //   .send(testUser)
+    //   expect(newTestUser.status).toEqual(200)
+    //   authToken = newTestUser.body
+    //   console.log(`authToken INSIDE CREATE tests have run: ${authToken}`)
+    // const authResponse = await request.post('/api/users/authenticate')
+    // .send(testUser)
+    // }) 
+    it('should log current authToken value to console', async () => {
+        console.log(`current authToken value:  ${authToken}`);
+        const decoded = jsonwebtoken_1.default.decode(authToken);
+        console.log(`here's decoded: ${decoded}`);
+        // const response = await request.post('/api/users/authenticate')
+        // .send(testUser)
+        // authToken = response.body
+        // console.log(`authToken right after CREATED INSIDE AUTHENTICATE: ${authToken}`)
+        // --- DECODE LOCALLY SCOPED AUTHTOKEN AND THEN PASS THAT DATA TO SEND METHOD ON AUTHENTICATE ENDPOINT
+        // --- PLUS SET MEHTOD TO AUTHORIZE NEW TEST USER DATA IN CONJUNCTION W RECENTLY GENERATED TOKEN ---//
+        // const decoded = jwt.verify(authToken, tokenSecret)
+        // console.log(`decoded is this: ${decoded}`)
+        // const response = await request.post('/api/users/authenticate')
+        // .send(testUser)
+        // console.log(`authToken INSIDE AUTHENTICATE tests have run: ${authToken}`)
+        // expect(response.status).toBe(200)
     });
 });
--- src/handlers/tests/orders_spec.ts
+++ src/handlers/tests/orders_spec.ts
@@ -7,12 +7,15 @@ import { OrderStore, OrderProduct} from '../../models/order'
 import { User, UserStore } from '../../models/user'
 
 dotenv.config()
-// const TokenSecret = process.env.authToken_SECRET as string
+const tokenSecret = process.env.TOKEN_SECRET as string
+console.log(`tokenSecret looks like this globally: ${tokenSecret}`)
 // const testOrderStore = new OrderStore()
 // const testUserStore = new UserStore()
 const request = supertest(app)
 
 // ------ TESTING CODE IDEAS FOR AUTHENTICATION TEST SOLUTIONS --------//
+
+
 describe('Order Enpoint Tests', () => {
   let authToken: string
   let testUser: User
@@ -22,26 +25,49 @@ describe('Order Enpoint Tests', () => {
       last_name: 'Hart', 
       first_name: 'Alan', 
       password: process.env.POSTGRES_TEST_PASSWORD as string
-    }
+    } as User
 
     const response = await request.post('/api/users')
     .send(testUser)
 
     authToken = response.body
+    console.log(`authToken BEFORE tests have run: ${authToken}`)
   })
 
-  it('should return 200 status for create user route', async () => {
-    const response = await request.post('/api/users')
-    .send(testUser)
-    expect(response.status).toEqual(200)
-    authToken = response.body
-  }) 
+  afterAll(async () => {
+    console.log(`authToken after tests have run: ${authToken}`)
+  })
 
-  it('should return 200 status for authenticate user route', async () => {
-    const response = await request.post('/api/users/authenticate')
-    .send(testUser)
-    .set('Authorization', authToken)
-        authToken = 'Bearer' + response.body
-    expect(response.status).toBe(200)
+  // it(`should return 200 status for create user route AND
+  //     should return 200 status for authenticate route`, async () => {
+  //   const newTestUser = await request.post('/api/users')
+  //   .send(testUser)
+  //   expect(newTestUser.status).toEqual(200)
+  //   authToken = newTestUser.body
+  //   console.log(`authToken INSIDE CREATE tests have run: ${authToken}`)
+
+    // const authResponse = await request.post('/api/users/authenticate')
+    // .send(testUser)
+  // }) 
+
+  it('should log current authToken value to console', async () => {
+    console.log(`current authToken value:  ${authToken}`)
+
+    const decoded = jwt.decode(authToken)
+    console.log(`here's decoded: ${decoded}`)
+    // const response = await request.post('/api/users/authenticate')
+    // .send(testUser)
+    // authToken = response.body
+    // console.log(`authToken right after CREATED INSIDE AUTHENTICATE: ${authToken}`)
+// --- DECODE LOCALLY SCOPED AUTHTOKEN AND THEN PASS THAT DATA TO SEND METHOD ON AUTHENTICATE ENDPOINT
+// --- PLUS SET MEHTOD TO AUTHORIZE NEW TEST USER DATA IN CONJUNCTION W RECENTLY GENERATED TOKEN ---//
+    // const decoded = jwt.verify(authToken, tokenSecret)
+    // console.log(`decoded is this: ${decoded}`)
+    // const response = await request.post('/api/users/authenticate')
+    // .send(testUser)
+    // console.log(`authToken INSIDE AUTHENTICATE tests have run: ${authToken}`)
+
+    // expect(response.status).toBe(200)
   })
 })
+
